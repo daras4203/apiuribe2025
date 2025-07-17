357849b7ed9dc8181fb8b53ba56bdab8374116f9
Fernando Arias <ferwin.arias@gmail.com>
2025-07-17 11:51:15 -0500

    ok

---
+++ apiRegistro/src/main/java/com/example/apiRegistro/ApiRegistroApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiRegistro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRegistroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRegistroApplication.class, args);
+	}
+
+}
